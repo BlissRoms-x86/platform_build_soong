40279dc2f5925d0cc2baa0c000c6e9a123f12188
hmtheboy154 <buingoc67@gmail.com>
2023-12-24 15:04:34 -0500

    Whitelist bindgen
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -107,6 +107,7 @@ var Configuration = map[string]PathConfig{
 	"llvm-objdump": Allowed,
 	"llvm-strip":   Allowed,
 	"rustc":   Allowed,
+	"bindgen":   Allowed,
 
 	// Host toolchain is removed. In-tree toolchain should be used instead.
 	// GCC also can't find cc1 with this implementation.
